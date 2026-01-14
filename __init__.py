1b090dc8b640d6bc69bd7fab6b929542606b9e88
WhiteWolf <matheodeponte45@gmail.com>
2026-01-14 11:32:49 +0100

    added syntaxe

--- __init__.py
+++ __init__.py
@@ -1,6 +1,11 @@
-from .audio import AudioManager
-from .data import DataManager
-from .time import TimeManager
-from .languages import LanguagesManager
-from .screen import ScreenManager
+from managers.audio import AudioManager
+from managers.data import DataManager
+from managers.time import TimeManager
+from managers.languages import LanguagesManager
+from managers.screen import ScreenManager
 __all__ = ["AudioManager", "DataManager", "TimeManager", "LanguagesManager", "ScreenManager"]
+
+"""
+Syntaxe d'import depuis GitHub :
+pip install git+https://github.com/WhiteWolf45380/pygame_managers.git
+"""
