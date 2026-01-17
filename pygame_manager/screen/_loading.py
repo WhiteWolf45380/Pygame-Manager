095c217105dc76506107fb72d628070d9c966303
WhiteWolf45380 <matheodeponte45@gmail.com>
2026-01-17 14:23:32 +0100

    added loading_frames

--- pygame_manager/screen/_loading.py
+++ pygame_manager/screen/_loading.py
@@ -4,7 +4,7 @@ import os
 # ======================================== GESTIONNAIRE ========================================
 class LoadingHandler:
     """Gestionnaire de l'animation de chargement"""
-    def __init__(self, screen, folder_path: str = "frames", text: str = "Chargement..."):
+    def __init__(self, screen, folder_path: str = "_loading_frames", text: str = "Chargement...", icon_size=(50, 50)):
         """
         Initialise le gestionnaire de loading
 
@@ -12,8 +12,10 @@ class LoadingHandler:
             - screen : surface pygame sur laquelle dessiner
             - folder_path (str) : chemin relatif vers le dossier des frames du logo
             - text (str) : texte de chargement
+            - icon_size (tuple) : taille (width, height) des frames de l'icône
         """
         self.__screen = screen
+        self.__icon_size = icon_size
 
         # chemin du module courant
         current_dir = os.path.dirname(__file__)
@@ -24,7 +26,10 @@ class LoadingHandler:
         for file in sorted(os.listdir(frames_dir)):
             if file.endswith(".png"):
                 path = os.path.join(frames_dir, file)
-                self.__frames.append(pygame.image.load(path).convert_alpha())
+                image = pygame.image.load(path).convert_alpha()
+                # redimensionner
+                image = pygame.transform.scale(image, self.__icon_size)
+                self.__frames.append(image)
 
         if not self.__frames:
             raise RuntimeError("Aucune frame trouvée pour le loader !")
@@ -69,7 +74,6 @@ class LoadingHandler:
         # passage à la frame suivante
         self.__current_frame = (self.__current_frame + 1) % len(self.__frames)
 
-        # mise à jour de l'affichage
         pygame.display.flip()  # ou pygame.display.update([text_rect, rect]) pour plus de performance
 
     def set_text(self, text: str):
@@ -84,15 +88,19 @@ class LoadingHandler:
         self.__text = text
         self.__text_surface = self.__font.render(self.__text, True, (255, 255, 255))
 
-    def set_icon(self, folder_path: str):
+    def set_icon(self, folder_path: str, icon_size=None):
         """
         Modifie l'icone de chargement
 
         Args :
             - folder_path (str) : chemin du dossier vers les frames de l'icone
+            - icon_size (tuple, optional) : nouvelle taille des frames (width, height)
         """
         if not isinstance(folder_path, str):
             self._raise_error('set_icon', 'Folder must be a folder path')
+
+        if icon_size:
+            self.__icon_size = icon_size
 
         current_dir = os.path.dirname(__file__)
         frames_dir = os.path.join(current_dir, folder_path)
@@ -102,7 +110,9 @@ class LoadingHandler:
         for file in sorted(os.listdir(frames_dir)):
             if file.endswith(".png"):
                 path = os.path.join(frames_dir, file)
-                self.__frames.append(pygame.image.load(path).convert_alpha())
+                image = pygame.image.load(path).convert_alpha()
+                image = pygame.transform.scale(image, self.__icon_size)
+                self.__frames.append(image)
 
         if not self.__frames:
             self._raise_error("set_icon", "Aucune frame trouvée dans ce dossier !")
