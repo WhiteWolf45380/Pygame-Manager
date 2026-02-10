7380351a94cd777585dd6fecc7358649961722bc
WhiteWolf <matheodeponte45@gmail.com>
2026-02-10 18:07:03 +0100

    chore(context): auto return

--- pygame_manager/context.py
+++ pygame_manager/context.py
@@ -1,21 +1,6 @@
-import sys
-from pathlib import Path
-
 class Context:
     def __init__(self):
-        self.engine = None
-
-        if getattr(sys, "_MEIPASS", None):
-            managers_dir = Path(sys._MEIPASS) / "pygame_manager" / "managers"
-        else:
-            managers_dir = Path(__file__).parent / "managers"
-
-        if managers_dir.exists():
-            for p in managers_dir.iterdir():
-                if p.is_dir() and not p.name.startswith("_") and (p / "__init__.py").exists():
-                    setattr(self, p.name, None)
-        else:
-            print(f"Warning: managers_dir {managers_dir} does not exist.")
+        pass
 
     def __getattr__(self, name):
-        raise AttributeError(f"context has no attribute {name!r}")
+        return None
